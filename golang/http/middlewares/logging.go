045e03223eae398ccc2c1413268ee8b2ac5f0407
Matei Busui <matei@busui.ro>
2024-05-22 14:36:52 +0300

    golang/http/middlewares: added logging middleware

--- golang/http/middlewares/logging.go
+++ golang/http/middlewares/logging.go
@@ -1,1 +1,42 @@
 package middlewares
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/AwareRO/libaware/golang/http/identity"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/rs/zerolog/log"
+)
+
+type ExtraField func(r *http.Request, params httprouter.Params) (string, string)
+
+func ipv4(r *http.Request) string {
+	ip := r.Header.Get("X-Forwarded-For")
+	if ip == "" {
+		ip = strings.Split(r.RemoteAddr, ":")[0]
+	}
+
+	return ip
+}
+
+func isCrawler(r *http.Request) string {
+	return fmt.Sprintf("%v", identity.NewMileusna()(r.UserAgent()))
+}
+
+func LogRequest(nextHandler httprouter.Handle, extras ...ExtraField) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+		logger := log.Info().
+			Str("endpoint", r.URL.String()).
+			Str("method", r.Method).
+			Str("ip", ipv4(r)).
+			Str("crawler", isCrawler(r))
+		for _, extra := range extras {
+			logger = logger.Str(extra(r, params))
+		}
+		logger.Msg("Got request")
+		nextHandler(w, r, params)
+	}
+}
